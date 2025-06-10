a610fd76ab8b6da243054296ca0cc8ee5ff0215e
apple19287 <syj8584@gmail.com>
2025-06-10 20:58:27 +0900

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const pool = mysql.createPool({
     connectionLimit: 10, // 최대 연결 개수
     host: process.env.DB_HOST,   // MySQL 서버 주소
     user: process.env.DB_USER,        // MySQL 사용자 이름
-    password: process.env.DB_PASSWORD, // ✨ 당신의 MySQL 비밀번호로 변경하세요! ✨
+    password: process.env.DB_PASSWORD, // 당신의 MySQL 비밀번호로 변경하세요! ✨
     database: process.env.DB_NAME // 사용할 데이터베이스 이름
 });
 
