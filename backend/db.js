7ff795e81090002b7910058bdd793c0e53e5fd97
cjfwls2639 <cjfwls2639@gmail.com>
2025-06-10 20:12:47 +0900

    env 연동

--- backend/db.js
+++ backend/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     connectionLimit: 10, // 최대 연결 개수
-    host: 'localhost',   // MySQL 서버 주소
-    user: 'root',        // MySQL 사용자 이름
-    password: '0000', // ✨ 당신의 MySQL 비밀번호로 변경하세요! ✨
-    database: 'test' // 사용할 데이터베이스 이름
+    host: process.env.DB_HOST,   // MySQL 서버 주소
+    user: process.env.DB_USER,        // MySQL 사용자 이름
+    password: process.env.DB_PASSWORD, // ✨ 당신의 MySQL 비밀번호로 변경하세요! ✨
+    database: process.env.DB_NAME // 사용할 데이터베이스 이름
 });
 
 // pool이 에러 발생 시 처리
